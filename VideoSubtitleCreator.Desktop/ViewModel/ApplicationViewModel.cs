eb4f5022fd38a1e20b08a5fe012c728e575b6c96
zngduong <zngduong@gmail.com>
2014-10-08 11:08:57 +0700

    design Library view

---
+++ VideoSubtitleCreator.Desktop/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoSubtitleCreator.Desktop.ViewModel
+{
+    public class ApplicationViewModel : ViewModelBase
+    {
+        #region fields
+        private static ApplicationViewModel _this = new ApplicationViewModel();
+
+        #endregion
+
+        #region constructor
+        public static ApplicationViewModel This
+        {
+            get { return _this; }
+        }
+        #endregion
+    }
+}
---
+++ VideoSubtitleCreator.Desktop/ViewModel/LibraryViewModel.cs
@@ -0,0 +1,46 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VideoSubtitleCreator.Desktop.ViewModel
+{
+    public class LibraryViewModel : PaneViewModel
+    {
+        public LibraryViewModel()
+        {
+            this.Title = "Library";
+            this.ContentId = "{Library_ContentId}";
+        }
+
+        #region CloseCommand
+        RelayCommand<object> _closeComand = null;
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (_closeComand == null)
+                {
+                    _closeComand = new RelayCommand<object>((p) => OnClose(), (p) => CanClose());
+                }
+
+                return _closeComand;
+            }
+        }
+
+        private bool CanClose()
+        {
+            return true;
+        }
+
+        private void OnClose()
+        {
+            //Workspace.This.Close(this);
+        }
+        
+        #endregion
+    }
+}
